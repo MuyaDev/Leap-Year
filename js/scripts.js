e635c2f9cfcb858fcc98cde751c4089d315c06cc
¨John <folnett@yahoo.co.uk>
2018-07-31 14:29:34 +0300

    add function refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 // Business-logic //
+/*
 var leapYear = function (year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};*/
+
+
+var leapYear = function (year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
